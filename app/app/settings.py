cd47813b3ab7f86d4e7aeb3fddaf8752eb2a097e
iulia <iulia.puiu@hotmail.com>
2023-01-17 10:52:54 +0200

    Create users endpoints

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
     'core',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +131,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
